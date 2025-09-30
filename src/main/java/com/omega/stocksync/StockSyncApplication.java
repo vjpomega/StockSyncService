b9e2a8f0ab94bb445c742dc69f60ecad767f46b2
vjpomega <vjpomega@gmail.com>
2025-09-30 11:12:45 +0800

    Initial Commit

---
+++ src/main/java/com/omega/stocksync/StockSyncApplication.java
@@ -0,0 +1,13 @@
+package com.omega.stocksync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omega/stocksync/StockSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega.stocksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
